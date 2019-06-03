9fb49cef8aeb18a00aeb243cc1bf2d40199616d8
WeijingZeng <zwjandhh@gmail.com>
2019-06-03 12:34:48 +0800

    optimized code

--- seamless-slides/main.js
+++ seamless-slides/main.js
@@ -1,18 +1,33 @@
-$('.images>img:nth-child(1)').addClass('current')
-$('.images>img:nth-child(2)').addClass('enter')
-$('.images>img:nth-child(3)').addClass('enter')
-$('.images>img:nth-child(4)').addClass('enter')
-$('.images>img:nth-child(5)').addClass('enter')
+init()
 let n = 1;
 setInterval(()=>{
-    $(`.images>img:nth-child(${checkN(n)})`).removeClass('current').addClass('leave')
-    .one('transitionend',(e)=>{
-        $(e.currentTarget).removeClass('leave').addClass('enter')
-    })
-    $(`.images>img:nth-child(${checkN(n
-        +1)})`).removeClass('enter').addClass('current')
+    toLeave($(`.images>img:nth-child(${checkN(n)})`))
+        .one('transitionend',(e)=>{
+            toEnter($(e.currentTarget))
+        })
+    toCurrent($(`.images>img:nth-child(${checkN(n
+        +1)})`))
     n = n+1
 },3000)
+
+function init(){
+    $('.images>img:nth-child(1)').addClass('current')
+        .siblings().addClass('enter')
+}
+
+function toEnter($node){
+    $node.removeClass('leave current').addClass('enter')
+}
+
+function toCurrent($node){
+    $node.removeClass('enter leave').addClass('current')
+}
+
+function toLeave($node){
+    $node.removeClass('current enter').addClass('leave')
+    //不return的话返回的是undefined，则没有办法再调用.one
+    return $node
+}
 
 function checkN(n){
     let len = $('.images>img').length
